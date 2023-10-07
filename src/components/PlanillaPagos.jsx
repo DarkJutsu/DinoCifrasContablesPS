d60725130ec6662979144428dad9d5a6bc500754
DarkJutsu <ulloajose34@gmail.com>
2023-10-07 03:15:57 -0600

    fix(PlanillaPagos.jsx):Corregir nombre de rutas

--- src/components/PlanillaPagos.jsx
+++ src/components/PlanillaPagos.jsx
@@ -171,7 +171,7 @@ export function PlanillaPagos() {
   return (
     <>
       <div className="w-fit">
-        <Link to="/DinoCifrasPS/detail" state={{ state: param }}>
+        <Link to="/DinoCifrasContablesPS/detail" state={{ state: param }}>
           <div className="flex items-center font-comfortaa text-slate-800 hover:text-orange-50 bg-transparent w-fit py-2 px-4 rounded-md cursor-pointer mb-5 mt-9 border border-slate-800 hover:bg-slate-600">
             <MdArrowBack className="text-2xl cursor-pointer mr-2" />
             <span className="text-md font-bold">Volver</span>
